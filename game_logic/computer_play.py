b1bb95d001e31662e52400d31e63319824ebf5f8
Luis Alonzo <39046449+doclaag@users.noreply.github.com>
2024-04-10 20:34:37 -0600

    Update computer_play.py

--- game_logic/computer_play.py
+++ game_logic/computer_play.py
@@ -5,41 +5,39 @@ class ComputerPlayer:
         self.game = game
 
     def get_best_move(self):
-        for player in ('O', 'X'):
+        # Verificar si hay una jugada ganadora o de bloqueo para el jugador actual y la computadora
+        for jugador in ('O', 'X'):
             for i in range(3):
-                if self.game.buttons[i][0]['text'] == self.game.buttons[i][1]['text'] == player and self.game.buttons[i][2]['text'] == ' ':
+                # Verifica filas para ganar o bloquear
+                if self.juego.botones[i][0]['text'] == self.juego.botones[i][1]['text'] == jugador and self.juego.botones[i][2]['text'] == ' ':
                     return i, 2
-                if self.game.buttons[i][0]['text'] == self.game.buttons[i][2]['text'] == player and self.game.buttons[i][1]['text'] == ' ':
+                if self.juego.botones[i][0]['text'] == self.juego.botones[i][2]['text'] == jugador and self.juego.botones[i][1]['text'] == ' ':
                     return i, 1
-                if self.game.buttons[i][1]['text'] == self.game.buttons[i][2]['text'] == player and self.game.buttons[i][0]['text'] == ' ':
+                if self.juego.botones[i][1]['text'] == self.juego.botones[i][2]['text'] == jugador and self.juego.botones[i][0]['text'] == ' ':
                     return i, 0
-                if self.game.buttons[0][i]['text'] == self.game.buttons[1][i]['text'] == player and self.game.buttons[2][i]['text'] == ' ':
+                # Verifica columnas para ganar o bloquear
+                if self.juego.botones[0][i]['text'] == self.juego.botones[1][i]['text'] == jugador and self.juego.botones[2][i]['text'] == ' ':
                     return 2, i
-                if self.game.buttons[0][i]['text'] == self.game.buttons[2][i]['text'] == player and self.game.buttons[1][i]['text'] == ' ':
+                if self.juego.botones[0][i]['text'] == self.juego.botones[2][i]['text'] == jugador and self.juego.botones[1][i]['text'] == ' ':
                     return 1, i
-                if self.game.buttons[1][i]['text'] == self.game.buttons[2][i]['text'] == player and self.game.buttons[0][i]['text'] == ' ':
+                if self.juego.botones[1][i]['text'] == self.juego.botones[2][i]['text'] == jugador and self.juego.botones[0][i]['text'] == ' ':
                     return 0, i
-                if self.game.buttons[0][0]['text'] == self.game.buttons[1][1]['text'] == player and self.game.buttons[2][2]['text'] == ' ':
+                # Verifica diagonales para ganar o bloquear
+                if self.juego.botones[0][0]['text'] == self.juego.botones[1][1]['text'] == jugador and self.juego.botones[2][2]['text'] == ' ':
                     return 2, 2
-                if self.game.buttons[0][2]['text'] == self.game.buttons[1][1]['text'] == player and self.game.buttons[2][0]['text'] == ' ':
+                if self.juego.botones[0][2]['text'] == self.juego.botones[1][1]['text'] == jugador and self.juego.botones[2][0]['text'] == ' ':
                     return 2, 0
-                if self.game.buttons[1][1]['text'] == self.game.buttons[2][2]['text'] == player and self.game.buttons[0][0]['text'] == ' ':
+                if self.juego.botones[1][1]['text'] == self.juego.botones[2][2]['text'] == jugador and self.juego.botones[0][0]['text'] == ' ':
                     return 0, 0
-                if self.game.buttons[2][0]['text'] == self.game.buttons[1][1]['text'] == player and self.game.buttons[0][2]['text'] == ' ':
+                if self.juego.botones[2][0]['text'] == self.juego.botones[1][1]['text'] == jugador and self.juego.botones[0][2]['text'] == ' ':
                     return 0, 2
-        for player in ('O', 'X'):
-            opponent = 'O' if player == 'X' else 'X'
-            for i in range(3):
-                if self.game.buttons[i][0]['text'] == self.game.buttons[i][1]['text'] == opponent and self.game.buttons[i][2]['text'] == ' ':
-                    return i, 2
-                if self.game.buttons[i][0]['text'] == self.game.buttons[i][2]['text'] == opponent and self.game.buttons[i][1]['text'] == ' ':
-                    return i, 1
-                if self.game.buttons[i][1]['text'] == self.game.buttons[i][2]['text'] == opponent and self.game.buttons[i][0]['text'] == ' ':
-                    return i, 0
-        if self.game.buttons[1][1]['text'] == ' ':
+        # Verifica si el centro está vacío
+        if self.juego.botones[1][1]['text'] == ' ':
             return 1, 1
+        # Si no hay jugadas ganadoras, de bloqueo o centro vacío, elige una casilla aleatoria
         for i in range(3):
             for j in range(3):
-                if self.game.buttons[i][j]['text'] == ' ':
+                if self.juego.botones[i][j]['text'] == ' ':
                     return i, j
+        # Si todas las casillas están ocupadas, elige una casilla aleatoria
         return randint(0, 2), randint(0, 2)
