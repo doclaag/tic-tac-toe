1461932eaf12c27901ce94b6d257a6738b8820e4
Luis Alonzo <39046449+doclaag@users.noreply.github.com>
2024-04-10 20:23:18 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import tkinter as tk
-from game_logic.tic_tac_toe import TicTacToe
+import tkinter as tk  
+from game_logic.tic_tac_toe import TicTacToe  
 
-if __name__ == '__main__':
-    root = tk.Tk()
+if __name__ == '__main__':  
+    root = tk.Tk()  
     root.title("Juego de Totito") 
-    game = TicTacToe(root)
-    root.mainloop()
+    game = TicTacToe(root) 
+    root.mainloop()  # Inicia el bucle principal de la interfaz gráfica de usuario
